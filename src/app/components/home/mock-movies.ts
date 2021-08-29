a427b4fb79881f387253517025cb8c6c3b69b7e0
Arani Shawkat <aranishawkat@gmail.com>
2021-08-29 01:15:42 -0400

    Add mock movies for rendering

---
+++ src/app/components/home/mock-movies.ts
@@ -0,0 +1,114 @@
+import { Movie } from "./movie";
+
+export const Movies: Movie[] = [
+  {
+    id: "1",
+    name: "PAW Patrol: The Movie",
+    rating: 5,
+    genre: "Animation, Family",
+    year: "2021",
+    poster:
+      "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ic0intvXZSfBlYPIvWXpU1ivUCO.jpg",
+    shortDescription:
+      "Ryder and the pups are called to Adventure City to stop Mayor Humdinger from turning the bustling metropolis into a state of chaos.",
+    longDescription:
+      "When the PAW Patrol's biggest rival, Mayor Humdinger, becomes Mayor of nearby Adventure City and starts wreaking havoc, Ryder and everyone's favorite heroic pups kick into high gear to face the challenge head-on. While one of the pups must face his past in Adventure City, the team finds help from a new ally, the savvy dachshund Liberty. Together, armed with exciting new gadgets and gear, the PAW Patrol fights to save the citizens of Adventure City and stop Mayor Humdinger from destroying the bustling metropolis.",
+    reviews: [
+      {
+        id: "1",
+        name: "Siransom",
+        rating: 4,
+        review:
+          "Took my son to see this this afternoon. Could not have been a better experience. Our son is autistic so we weren't sure how he'd react sitting still for so long... He sat, for the entirety of the movie! He clapped, he got excited in certain parts but most importantly he sat still eating his popcorn from beginning to end!! 😀 very, very proud parents here. I love all things cinema, film and TV so this means so much to me.",
+      },
+    ],
+  },
+  {
+    id: "2",
+    name: "Jungle Cruise",
+    rating: 4,
+    genre: "Animation, Family",
+    year: "2021",
+    poster:
+      "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9dKCd55IuTT5QRs989m9Qlb7d2B.jpg",
+    shortDescription:
+      "Ryder and the pups are called to Adventure City to stop Mayor Humdinger from turning the bustling metropolis into a state of chaos.",
+    longDescription:
+      "When the PAW Patrol's biggest rival, Mayor Humdinger, becomes Mayor of nearby Adventure City and starts wreaking havoc, Ryder and everyone's favorite heroic pups kick into high gear to face the challenge head-on. While one of the pups must face his past in Adventure City, the team finds help from a new ally, the savvy dachshund Liberty. Together, armed with exciting new gadgets and gear, the PAW Patrol fights to save the citizens of Adventure City and stop Mayor Humdinger from destroying the bustling metropolis.",
+    reviews: [
+      {
+        id: "1",
+        name: "Siransom",
+        rating: 4,
+        review:
+          "Took my son to see this this afternoon. Could not have been a better experience. Our son is autistic so we weren't sure how he'd react sitting still for so long... He sat, for the entirety of the movie! He clapped, he got excited in certain parts but most importantly he sat still eating his popcorn from beginning to end!! 😀 very, very proud parents here. I love all things cinema, film and TV so this means so much to me.",
+      },
+    ],
+  },
+  {
+    id: "3",
+    name: "PAW Patrol: The Movie",
+    rating: 4,
+    genre: "Animation, Family, Adventure, Comedy",
+    year: "2021",
+    poster:
+      "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ic0intvXZSfBlYPIvWXpU1ivUCO.jpg",
+    shortDescription:
+      "Ryder and the pups are called to Adventure City to stop Mayor Humdinger from turning the bustling metropolis into a state of chaos.",
+    longDescription:
+      "When the PAW Patrol's biggest rival, Mayor Humdinger, becomes Mayor of nearby Adventure City and starts wreaking havoc, Ryder and everyone's favorite heroic pups kick into high gear to face the challenge head-on. While one of the pups must face his past in Adventure City, the team finds help from a new ally, the savvy dachshund Liberty. Together, armed with exciting new gadgets and gear, the PAW Patrol fights to save the citizens of Adventure City and stop Mayor Humdinger from destroying the bustling metropolis.",
+    reviews: [
+      {
+        id: "1",
+        name: "Siransom",
+        rating: 4,
+        review:
+          "Took my son to see this this afternoon. Could not have been a better experience. Our son is autistic so we weren't sure how he'd react sitting still for so long... He sat, for the entirety of the movie! He clapped, he got excited in certain parts but most importantly he sat still eating his popcorn from beginning to end!! 😀 very, very proud parents here. I love all things cinema, film and TV so this means so much to me.",
+      },
+    ],
+  },
+  {
+    id: "4",
+    name: "PAW Patrol: The Movie",
+    rating: 4,
+    genre: "Animation, Family, Adventure, Comedy",
+    year: "2021",
+    poster:
+      "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ic0intvXZSfBlYPIvWXpU1ivUCO.jpg",
+    shortDescription:
+      "Ryder and the pups are called to Adventure City to stop Mayor Humdinger from turning the bustling metropolis into a state of chaos.",
+    longDescription:
+      "When the PAW Patrol's biggest rival, Mayor Humdinger, becomes Mayor of nearby Adventure City and starts wreaking havoc, Ryder and everyone's favorite heroic pups kick into high gear to face the challenge head-on. While one of the pups must face his past in Adventure City, the team finds help from a new ally, the savvy dachshund Liberty. Together, armed with exciting new gadgets and gear, the PAW Patrol fights to save the citizens of Adventure City and stop Mayor Humdinger from destroying the bustling metropolis.",
+    reviews: [
+      {
+        id: "1",
+        name: "Siransom",
+        rating: 4,
+        review:
+          "Took my son to see this this afternoon. Could not have been a better experience. Our son is autistic so we weren't sure how he'd react sitting still for so long... He sat, for the entirety of the movie! He clapped, he got excited in certain parts but most importantly he sat still eating his popcorn from beginning to end!! 😀 very, very proud parents here. I love all things cinema, film and TV so this means so much to me.",
+      },
+    ],
+  },
+  {
+    id: "5",
+    name: "PAW Patrol: The Movie",
+    rating: 3,
+    genre: "Animation, Family, Adventure, Comedy",
+    year: "2021",
+    poster:
+      "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ic0intvXZSfBlYPIvWXpU1ivUCO.jpg",
+    shortDescription:
+      "Ryder and the pups are called to Adventure City to stop Mayor Humdinger from turning the bustling metropolis into a state of chaos.",
+    longDescription:
+      "When the PAW Patrol's biggest rival, Mayor Humdinger, becomes Mayor of nearby Adventure City and starts wreaking havoc, Ryder and everyone's favorite heroic pups kick into high gear to face the challenge head-on. While one of the pups must face his past in Adventure City, the team finds help from a new ally, the savvy dachshund Liberty. Together, armed with exciting new gadgets and gear, the PAW Patrol fights to save the citizens of Adventure City and stop Mayor Humdinger from destroying the bustling metropolis.",
+    reviews: [
+      {
+        id: "1",
+        name: "Siransom",
+        rating: 4,
+        review:
+          "Took my son to see this this afternoon. Could not have been a better experience. Our son is autistic so we weren't sure how he'd react sitting still for so long... He sat, for the entirety of the movie! He clapped, he got excited in certain parts but most importantly he sat still eating his popcorn from beginning to end!! 😀 very, very proud parents here. I love all things cinema, film and TV so this means so much to me.",
+      },
+    ],
+  },
+];
